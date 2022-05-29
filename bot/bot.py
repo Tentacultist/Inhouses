7c7269672bf001710cf1051d9dad7fa2c97903c6
Tentacultist <lucsyle@gmail.com>
2022-05-29 18:08:24 -0400

    match

--- bot/bot.py
+++ bot/bot.py
@@ -73,7 +73,7 @@ async def createLobby(ctx):
     timestr = datetime.now().strftime("%H:%M")
     description = "5v5 Lobby created at " + timestr + " on " +  datestr + "\n\n**For Players**\nTo join, react with ✅\nTo leave, react with ❌\n\n**For Lobby Leader**\nTo close the lobby, react with 😭\n To begin match, react with 💢\n"
     
-    embed=discord.Embed(title="League 5v5" , url="https://github.com/Tentacultist/Inhouses", description=description, color=0x006cfa)
+    embed=discord.Embed(title="League 5v5 Lobby" , url="https://github.com/Tentacultist/Inhouses", description=description, color=0x006cfa)
     embed.set_author(name=ctx.message.author.name, icon_url=ctx.author.avatar_url)
     embed.set_thumbnail(url="https://pentagram-production.imgix.net/cc7fa9e7-bf44-4438-a132-6df2b9664660/EMO_LOL_02.jpg?rect=0%2C0%2C1440%2C1512&w=640&crop=1&fm=jpg&q=70&auto=format&fit=crop&h=672")
     
@@ -169,7 +169,73 @@ async def createLobby(ctx):
 
         # splits list of players into 2 and then displays
         if str(reaction[0]) == '💢' and userid == ctx.author.id:
-            await ctx.send(":^)")
+            
+            #if playeridlist[9] == 0:
+
+                #await ctx.send("There are not enough players in the queue")
+                #await msg.remove_reaction(reaction[0], user)
+                #continue
+
+            playerRVList = []
+
+            for id in playeridlist:
+                if(id != 0):
+                    playerRVList.append(rws.rankValue(jsu.getRank(id)))
+
+            # returns 2 teams of relatively equal strength, the player ids
+            teamOne,teamTwo = util.splitTeams(playeridlist, playerRVList)
+
+            print(teamOne)
+            print(teamTwo)
+
+            for emoji in emojis:
+                await msg.clear_reaction(emoji)
+            
+            gameEmojis = ["✋", "🟦", "🟥"]
+
+            for emoji in gameEmojis:
+                await msg.add_reaction(emoji)
+
+
+            gameDescription = "**Match Start!** Good luck and have fun\n\n**For Lobby Leader**\nTo cancel match, react with ✋\n Blue team win, 🟦\nRed team win, 🟥"
+
+            embedGame=discord.Embed(title="League 5v5 Match" , url="https://github.com/Tentacultist/Inhouses", description=gameDescription, color=0x006cfa)
+            embedGame.set_author(name=ctx.message.author.name, icon_url=ctx.author.avatar_url)
+            embedGame.set_thumbnail(url="https://pentagram-production.imgix.net/cc7fa9e7-bf44-4438-a132-6df2b9664660/EMO_LOL_02.jpg?rect=0%2C0%2C1440%2C1512&w=640&crop=1&fm=jpg&q=70&auto=format&fit=crop&h=672")
+            #embedGame.add_field(name="Team One", value="{}\n{}\n{}\n{}\n{}".format(*teamOne), inline=True)
+            #embedGame.add_field(name="Team Two", value="{}\n{}\n{}\n{}\n{}".format(*teamTwo), inline=True)
+
+            list1 = ["*Guy 1", "*Guy 2", "*Guy 3", "*Guy 4", "*Guy 5"]
+            list2 = ["*Guy 6", "*Guy 7", "*Guy 8", "*Guy 9", "*Guy 10"]
+            embedGame.add_field(name="Blue Team", value="{}\n{}\n{}\n{}\n{}".format(*list1), inline=True)
+            embedGame.add_field(name="Red Team", value="{}\n{}\n{}\n{}\n{}".format(*list2), inline=True)
+
+            await msg.edit(embed=embedGame)
+
+            while(True):
+
+                reaction = await bot.wait_for("reaction_add", check=checkReaction)
+
+                if str(reaction[0]) == '✋' and userid == ctx.author.id:
+            
+                    descriptionMatchClosed = "Match has been canceled, Touch Grass"
+                    embedClosed = discord.Embed(title="Match Closed", description=descriptionMatchClosed)
+                    await msg.edit(embed=embedClosed)
+
+                    for emoji in gameEmojis:
+                        await msg.clear_reaction(emoji)
+
+                    return
+                
+                
+                        
+
+
+            
+
+
+
+    
 
     # at any number of people, will split the players in half based on rank lp, need to make module to determine rank/number associated, use algo to return
 
